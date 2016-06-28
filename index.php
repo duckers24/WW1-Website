9c9b623a6942ac8ded03d357581823162168eb55
William Duckworth <will@wduckworth.me.uk>
2016-06-28 23:32:45 +0100

    Created Index Page
    I created the index page and copied the code over from the original
    website

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<Title>Home</title>
+    <link href="Style.css" type = "text/css" rel="stylesheet"/>
+</head>
+<body class="Body">
+    <div class="Container">
+    <div class="Header">
+        <h1 class="Body">World War 1</h1>
+    </div>
+    <?php
+        include 'includes/year_menu.txt'
+    ?>
+    <?php
+        include 'includes/homepage.txt'
+    ?>        
+    </div>
+</body> 
+</html>
