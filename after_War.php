158a9596548f66b2037138d556e3a47fc7cecab3
William Duckworth <will@wduckworth.me.uk>
2016-06-29 10:47:41 +0100

    Template Code
    I copied the code across from the template

--- after_War.php
+++ after_War.php
@@ -1,26 +1,94 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<Title>After the War</title>
-    <link href="includes/style.css" type = "text/css" rel="stylesheet"/>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bare - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <style>
+    body {
+        padding-top: 70px;
+        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+    }
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
-<body class="Body">
-    <div class="Container">
-    <div class="Header">
-        <h1 class="Body">After the War</h1>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Start Bootstrap</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>A Bootstrap Starter Template</h1>
+                <p class="lead">Complete with pre-defined file paths that you won't have to change!</p>
+                <ul class="list-unstyled">
+                    <li>Bootstrap v3.3.6</li>
+                    <li>jQuery v1.11.1</li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.row -->
+
     </div>
-    <?php
-        include 'includes/year_menu.txt'
-    ?>
-        <p1>This can be used to see all of the soldiers that died after the war.</p1>  
-        <Table class="MemorialTable">
-            <tr>
-                <td class="MemorialTableLeft"><span title="View all of the soldiers that died in 1919"><a href='year.php?year=1919'><img src="graphics/after_War/1919Small.jpg"></a></span></td>
-                <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableCenter"><span title="View all of the soldiers that died in 1920"><a href='year.php?year=1920'><img src="graphics/after_War/1920Small.jpg"></a></span></td>
-                <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableRight"><span title="View all of the soldiers that died in 1928"><a href='year.php?year=1928'><img src="graphics/after_War/1928Small.jpg"></a></span></td>    
-            </tr>
-        </Table>  
-    </div>   
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
 </body>
+
 </html>
