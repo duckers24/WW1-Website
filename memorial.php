8f102ecfc8f424155e9e39cbcbc817c0c9e04761
William Duckworth <will@wduckworth.me.uk>
2016-06-29 01:21:28 +0100

    Page Links
    I updated the links to the correct page

--- memorial.php
+++ memorial.php
@@ -16,31 +16,31 @@
     ?>
         <Table class="MemorialTable">
             <tr>
-                <td class="MemorialTableLeft"><span title="All the information about the Amberley Memorial"><a href='MemorialPage.php?Memorial=Amberley'><img src="graphics/memorial_Button/AmberleySmall.jpg"></a></span></td>
+                <td class="MemorialTableLeft"><span title="All the information about the Amberley Memorial"><a href='memorial_Page.php?Memorial=Amberley'><img src="graphics/memorial_Button/AmberleySmall.jpg"></a></span></td>
                 <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableCenter"><span title="All the information about the Avening Memorial"><a href='MemorialPage.php?Memorial=Avening'><img src="graphics/memorial_Button/AveningSmall.jpg"></a></span></td>
+                <td class="MemorialTableCenter"><span title="All the information about the Avening Memorial"><a href='memorial_Page.php?Memorial=Avening'><img src="graphics/memorial_Button/AveningSmall.jpg"></a></span></td>
                 <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableRight"><span title="All the information about the Forest Green Memorial"><a href='MemorialPage.php?Memorial=Forest'><img src="graphics/memorial_Button/ForestGreenSmall.jpg"></a></span></td>    
+                <td class="MemorialTableRight"><span title="All the information about the Forest Green Memorial"><a href='memorial_Page.php?Memorial=Forest'><img src="graphics/memorial_Button/ForestGreenSmall.jpg"></a></span></td>    
             </tr>
             <tr class="MemorialTableGap"></tr>
             <tr>
-                <td class="MemorialTableLeft"><span title="All the information about the Horsley Memorial"><a href='MemorialPage.php?Memorial=Hosrley'><img src="graphics/memorial_Button/HorsleySmall.jpg"></a></span></td>
+                <td class="MemorialTableLeft"><span title="All the information about the Horsley Memorial"><a href='memorial_Page.php?Memorial=Horsley'><img src="graphics/memorial_Button/HorsleySmall.jpg"></a></span></td>
                 <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableCenter"><span title="All the information about the Minchinhampton Memorial"><a href='MemorialPage.php?Memorial=Minchinhampton'><img src="graphics/memorial_Button/MinchinhamptonSmall.jpg"></a></span></td>
+                <td class="MemorialTableCenter"><span title="All the information about the Minchinhampton Memorial"><a href='memorial_Page.php?Memorial=Minchinhampton'><img src="graphics/memorial_Button/MinchinhamptonSmall.jpg"></a></span></td>
                 <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableRight"><span title="All the information about the Nympsfield Memorial"><a href='MemorialPage.php?Memorial=Nympsfield'><img src="graphics/memorial_Button/NympsfieldSmall.jpg"></a></span></td>    
+                <td class="MemorialTableRight"><span title="All the information about the Nympsfield Memorial"><a href='memorial_Page.php?Memorial=Nympsfield'><img src="graphics/memorial_Button/NympsfieldSmall.jpg"></a></span></td>    
             </tr>
             <tr class="MemorialTableGap"></tr>
             <tr>
-                <td class="MemorialTableLeft"><span title="All the information about the Rodborough Memorial"><a href='MemorialPage.php?Memorial=Rodborough'><img src="graphics/memorial_Button/RodboroughSmall.jpg"></a></span></td>
+                <td class="MemorialTableLeft"><span title="All the information about the Rodborough Memorial"><a href='memorial_Page.php?Memorial=Rodborough'><img src="graphics/memorial_Button/RodboroughSmall.jpg"></a></span></td>
                 <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableCenter"><span title="All the information about the Shortwood Memorial"><a href='MemorialPage.php?Memorial=Shortwood'><img src="graphics/memorial_Button/ShortwoodSmall.jpg"></a></span></td>
+                <td class="MemorialTableCenter"><span title="All the information about the Shortwood Memorial"><a href='memorial_Page.php?Memorial=Shortwood'><img src="graphics/memorial_Button/ShortwoodSmall.jpg"></a></span></td>
                 <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableRight"><span title="All the information about the Gloters Memorial"><a href='MemorialPage.php?Memorial=Gloters'><img src="graphics/memorial_Button/GlotersSmall.jpg"></a></span></td>    
+                <td class="MemorialTableRight"><span title="All the information about the Gloters Memorial"><a href='memorial_Page.php?Memorial=Gloters'><img src="graphics/memorial_Button/GlotersSmall.jpg"></a></span></td>    
             </tr>
             <tr class="MemorialTableGap"></tr>
             <tr>
-                <td class="MemorialTableLeft"><span title="All the information about the Woodchester Memorial"><a href='MemorialPage.php?Memorial=Woodchester'><img src="graphics/memorial_Button/WoodchesterSmall.jpg"></a></span></td>
+                <td class="MemorialTableLeft"><span title="All the information about the Woodchester Memorial"><a href='memorial_Page.php?Memorial=Wodchester'><img src="graphics/memorial_Button/WoodchesterSmall.jpg"></a></span></td>
             </tr>
             <tr class="MemorialTableGap"></tr>
         </Table>
