0aa28576df5aff1bcd6ec0f6f865081181d2375e
William Duckworth <will@wduckworth.me.uk>
2016-06-29 10:41:08 +0100

    Title
    I removed all of the old code and then changed the title

--- during_War.php
+++ during_War.php
@@ -1,38 +1,3 @@
-<html>
-<head>
-<Title>During the War</title>
-    <link href="includes/style.css" type = "text/css" rel="stylesheet"/>
-</head>
-<body class="Body">
-    <div class="Container">
-    <div class="Header">
-        <h1 class="Body">During the War</h1>
-    </div>
-    <?php
-        include 'includes/year_menu.txt'
-    ?>
-        <p1 class="Text">This shows all of the soliders that lost their life fighing in the World War. You are able to see who died, and the year that they died in.</p1>
-        <Table class="MemorialTable">
-            <tr>
-                <td class="MemorialTableLeft"><span title="See all of the soldiers that lost their lives in 1914"><a href='year.php?year=1914'><img src="graphics/during_War/1914Small.jpg"></a></span></td>
-                <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableCenter"><span title="See all of the soldiers that lost their lives in 1915"><a href='year.php?year=1915'><img src="graphics/during_War/1915Small.jpg"></a></span></td>
-                <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableRight"><span title="See all of the soldiers that lost their lives in 1916"><a href='year.php?year=1916'><img src="graphics/during_War/1916Small.jpg"></a></span></td>    
-            </tr>
-            <tr class="MemorialTableGap"></tr>
-            <tr>
-                <td class="MemorialTableLeft"><span title="See all of the soldiers that lost their lives in 1917"><a href='year.php?year=1917'><img src="graphics/during_War/1917Small.jpg"></a></span></td>
-                <td class="MemorialTableMargin"></td>
-                <td class="MemorialTableCenter"><span title="See all of the soldiers that lost their lives in 1918"><a href='year.php?year=1918'><img src="graphics/during_War/1918Small.jpg"></a></span></td>
-                <td class="MemorialTableMargin"></td>    
-            </tr>
-            <tr class="MemorialTableGap"></tr>
-        </Table>
-    </div>
-</body> 
-</html>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -44,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Bare - Start Bootstrap Template</title>
+    <title>During the War</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="includes/css/bootstrap.min.css" rel="stylesheet">
