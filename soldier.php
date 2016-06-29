42cde23c35d0497933b18859140da9b56e028434
William Duckworth <will@wduckworth.me.uk>
2016-06-29 12:20:50 +0100

    Moved Database Call
    I moved the call to the database down the page and the heading will now
    display the soldiers name

--- soldier.php
+++ soldier.php
@@ -16,11 +16,8 @@
     
     <?php
     $Sequence=$_GET['Sequence']
-    // Request the text of soldiers
-    $result = @mysql_query('SELECT Sequence, First_name, Last_name, Number, Rank, Regiment, Awards, Year_born, How_died, Where_died, Where_buried, Where_commemorated, Birthplace FROM soldiers WHERE Sequence = "'.$Sequence.'" ORDER by Last_name');
-    if (!$result) {
-        exit('<p>Error performing query: ' . mysql_error() . '</p>');
-    }
+        ?>
+    <?php
     include 'includes/db.inc.php'
     ?>
     
@@ -81,6 +78,11 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <?php
+        // Request the text of soldiers
+    $result = @mysql_query('SELECT Sequence, First_name, Last_name, Number, Rank, Regiment, Awards, Year_born, How_died, Where_died, Where_buried, Where_commemorated, Birthplace FROM soldiers WHERE Sequence = "'.$Sequence.'" ORDER by Last_name');
+    if (!$result) {
+        exit('<p>Error performing query: ' . mysql_error() . '</p>');
+    }
         while ($Name = mysql_fetch_array($result))
         {
             echo '<h1>'. $Name['First_name'] . " " . $Name['Last_name'] .'</h1>';
