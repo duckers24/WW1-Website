2684e0ba95e772012926699124bc49b8316ee5e1
William Duckworth <will@wduckworth.me.uk>
2016-06-29 14:36:21 +0100

    Search Box
    I added the search box to the navigation to be displayed at the top of
    the page

--- search_Results.php
+++ search_Results.php
@@ -71,6 +71,16 @@ include 'includes/db.inc.php'
                         <a href="events.php">Events</a>
                     </li>
                 </ul>
+                <div class="col-sm-3 col-md-3 pull-right">
+                    <form class="navbar-form" role="search" method="get" action="search_Results.php">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
             <!-- /.navbar-collapse -->
         </div>
