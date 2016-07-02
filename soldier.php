bcaa4a3dbb82323319a164e14979f1d4ceac407b
William Duckworth <will@wduckworth.me.uk>
2016-07-02 13:38:02 +0100

    Javascript Includes
    I added the includes to the javascript files that I have added

--- soldier.php
+++ soldier.php
@@ -181,6 +181,11 @@ $Sequence=$_GET['Sequence']
     <!-- Bootstrap Core JavaScript -->
     <script src="includes/js/bootstrap.min.js"></script>
     
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script type="text/javascript" src="includes/js/fliplightbox.min.js"></script>
+	<script type="text/javascript">$('#graphics').flipLightBox()</script>
 </body>
 
 </html>
