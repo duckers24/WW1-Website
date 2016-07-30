ca6ce89cb1e04c7c6037c037ba769fbd045c8732
Cali-cat <jonathan@MBP-Retina.local>
2016-07-30 20:07:25 +0100

    New database connection
    Database now on NTC server

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,6 +1,6 @@
 <?php
 //  $dbcnx = @mysql_connect('127.0.0.1', 'baltinai_admin', 'vent8en7if7');
-  $dbcnx = @mysql_connect('localhost', 'root', 'root');
+  $dbcnx = @mysql_connect('83.223.125.192', 'nailswor_world', 'TbiQWRKGKJEY4N');
   if (!$dbcnx) {
     exit('<p>Unable to connect to the ' .
         'database server at this time.</p>');
